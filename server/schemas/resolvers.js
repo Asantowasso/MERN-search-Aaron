87bf8c2c1b7cbfc39303ad26297d06133262add2
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-01-22 13:00:26 -0500

    added code to resolvers allowing a user to login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
-const {Book, User} = require ('.../models')
+const {Book, User} = require ('.../models');
+const { User } = require('../models');
 
 const resolvers = {
   Query: {
@@ -7,8 +8,31 @@ const resolvers = {
 
   Mutation: {
     addUser: async (parent, {username, email, password}) => {
-      return await User.create ({username, email, password})
+      const User = await User.create({username, email, password});
+      const token = signToken(User);
+
+      return{token, User};
+    },
+    login: async(parent, {username, password}) => {
+      const User = await User.findOne({
+        username
+      });
+
+      if (!User){
+        throw new AuthenticationError('No User with this login found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password')
+      }
+
+      const token = signToken(profile);
+      return {token, profile};
+
     }
+
   }
   
 
